b25efedf01c7cb5db4a4c2bb8d3984a605666164
lttb <kenzhaev.artur@gmail.com>
2019-03-13 04:15:52 +0300

    fix

--- src/runtime/vue/index.js
+++ src/runtime/vue/index.js
@@ -1,15 +1,19 @@
-const {map: _map, ...rest} = require('..');
+import styled, {map as _map, use, css, create, set, __css__} from '..';
 
-rest.default.classProp = 'class';
+styled.classProp = 'class';
 
 function map(element, data) {
     if (!data.attrs) return data;
     data.attrs.class = data.class;
+    data.attrs.style = data.style;
     data.attrs = _map(element, data.attrs);
     data.class = data.attrs.class;
+    data.style = data.attrs.style;
     delete data.attrs.class;
+    delete data.attrs.style;
     return data;
 }
 
-module.exports = rest;
-module.exports.map = map;
+export {use, css, create, set, map, __css__};
+
+export default styled;
