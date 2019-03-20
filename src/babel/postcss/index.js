4969b8ff672d13c9e5631a41f62247844832abf9
lttb <kenzhaev.artur@gmail.com>
2019-03-20 09:35:49 +0300

    add postcss-preset-env and cssnano

--- src/babel/postcss/index.js
+++ src/babel/postcss/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const postcss = require('postcss');
-const nesting = require('postcss-nesting');
+const postcssPresetEnv = require('postcss-preset-env');
 const atImport = require('postcss-import-sync2');
 
 const localByDefault = require('postcss-modules-local-by-default');
@@ -37,11 +37,26 @@ function getScopedNameGenerator(opts) {
     return genericNames(scopedNameGenerator, {context: process.cwd()});
 }
 
-/* css-modules */
+/**
+ * Babel is synchronous, so we need to use synchronous plugins
+ * We can use something like `deasync` and other hacks, but it's not stable and platform-agnostic
+ * The most of the postcss plugin features are sync (not to mention `fs` and so on),
+ * and this wrapper should cover the most of the use cases for `preset-env`, for example
+ *
+ * Anyway it's highly recommended to use `reshadow/webpack` + `reshadow/postcss`
+ */
+const syncPlugin = plugin => (root, result) => {
+    const {SynchronousPromise} = require('synchronous-promise');
+    const realPromise = global.Promise;
+    global.Promise = SynchronousPromise;
+    plugin(root, result);
+    global.Promise = realPromise;
+};
 
 module.exports = ({plugins = [], options = {}, generateScopedName}) => {
     generateScopedName = getScopedNameGenerator({generateScopedName});
 
+    /* css-modules */
     const modulesPlugins = [
         values,
         localByDefault,
@@ -65,17 +80,35 @@ module.exports = ({plugins = [], options = {}, generateScopedName}) => {
 
     const parser = new Parser(pathFetcher);
 
-    const processor = postcss([
+    const processorPlugins = [
         atImport({
             ...options.import,
             sync: true,
         }),
         ...plugins,
-        nesting(options.nesting),
+        syncPlugin(
+            postcssPresetEnv(
+                options.presetEnv || {
+                    features: {
+                        'nesting-rules': true,
+                    },
+                },
+            ),
+        ),
         reshadow(options.reshadow),
         ...modulesPlugins,
         parser.plugin,
-    ]);
+    ];
+
+    if (options.cssnano || process.env.NODE_ENV === 'production') {
+        /**
+         * `preset` option is required to force `cssnano` use `preset` istead of config search
+         */
+        const nanoOptions = Object.assign({preset: 'default'}, options.cssnano);
+        processorPlugins.push(syncPlugin(require('cssnano')(nanoOptions)));
+    }
+
+    const processor = postcss(processorPlugins);
 
     const load = (css, options = {}) => {
         if (options.from in cache) return cache[options.from];
