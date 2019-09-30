ab5b2f39febf3a79da3551a7ffb3425485892f4b
lttb <kenzhaev.artur@gmail.com>
2019-09-30 18:40:31 +0300

    feat(reshadow): add strict elements.d.ts for custom jsx elements

---
+++ packages/reshadow/elements.d.ts
@@ -0,0 +1,9 @@
+type CustomElement<T> = T extends any
+  ? ({ as: T } & JSX.IntrinsicElements[T])
+  : never;
+
+declare namespace JSX {
+  interface IntrinsicElements extends JSX.IntrinsicElements {
+    [name: string]: CustomElement<keyof JSX.IntrinsicElements>;
+  }
+}
