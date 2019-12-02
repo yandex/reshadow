62e133774700663c8c166900ae823c2e8bbc35b0
lttb <kenzhaev.artur@gmail.com>
2019-12-02 20:19:45 +0300

    feat(react): add types for helpers

--- packages/react/index.d.ts
+++ packages/react/index.d.ts
@@ -1,33 +1,37 @@
-import { createElement, Component } from 'react';
+import {createElement, Component} from 'react';
 
 declare function id<A extends JSX.Element>(node: A): A;
 
 declare function taggedStyled(
-  strs: TemplateStringsArray,
-  ...values: any[]
+    strs: TemplateStringsArray,
+    ...values: any[]
 ): typeof id;
 declare function taggedStyled<A extends JSX.Element>(node: A): A;
 
-declare function styled<S extends { [key: string]: string }>(
-  ...styles: S[]
+declare function styled<S extends {[key: string]: string}>(
+    ...styles: S[]
 ): typeof taggedStyled;
 declare function styled(
-  strs: TemplateStringsArray,
-  ...values: any[]
+    strs: TemplateStringsArray,
+    ...values: any[]
 ): typeof id;
 
-export declare var use: (<S extends { [key: string]: any }>(value: S) => {}) & {
-  [key: string]: typeof Component;
+export declare var use: (<S extends {[key: string]: any}>(value: S) => {}) & {
+    [key: string]: typeof Component;
 };
 
 export declare function css(
-  strs: TemplateStringsArray,
-  ...values: any[]
-): { [key: string]: string };
+    strs: TemplateStringsArray,
+    ...values: any[]
+): {[key: string]: string};
 export declare function keyframes(
-  strs: TemplateStringsArray,
-  ...values: any[]
+    strs: TemplateStringsArray,
+    ...values: any[]
 ): string;
+
+/** TODO: improve types for helpers */
+export declare function create(styles: object[]): {[key: string]: string};
+export declare function map(name: string, ...args: object[]): object;
 
 declare var jsx: typeof createElement;
 
